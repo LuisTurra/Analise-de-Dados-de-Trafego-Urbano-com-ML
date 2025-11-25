baf2630b9343beba37bcfcbb3561d7984202a2d9
Luis Henrique Turra Ramos <86019125+LuisTurra@users.noreply.github.com>
2025-11-25 13:10:39 -0300

    VFINALLLLLLLLLLL

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from dashboard import run_dashboard
+from .dashboard import app, run_dashboard
 from model import train_model
 from etl import etl_process
 
@@ -6,7 +6,7 @@ app_server = None
 
 if __name__ == "__main__":
     print("Iniciando ETL...")
-    df = etl_process("../data/traffic_data.csv")
+    df = etl_process("data/traffic_data.csv")
     print("Treinando modelo...")
     model, predictions = train_model(df)
     print("Gerando explicabilidade SHAP...")
--- test/test_etl.py
+++ test/test_etl.py
@@ -1,5 +1,5 @@
 import pytest
-from src.etl import etl_process, spark
+from app.etl import etl_process, spark
 
 def test_etl_process():
     file_path = "../data/traffic_data.csv"
@@ -8,4 +8,3 @@ def test_etl_process():
     assert "features" in df.columns, "Coluna de features ausente"
     assert "label" in df.columns, "Coluna de label ausente"
 
-# Rode com: pytest tests/test_etl.py
