13826bd9ebc45b114bd4e48e739bcb413f7aa4c4
Luis Henrique Turra Ramos <86019125+LuisTurra@users.noreply.github.com>
2025-11-25 14:59:12 -0300

    tired chief

--- app/main.py
+++ app/main.py
@@ -1,17 +1,26 @@
 import sys
 import os
+import pyspark
+from pyspark.sql import SparkSession
+
+
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+
 from dashboard import app, run_dashboard
+from etl import etl_process 
 from model import train_model, explain_model
-from etl import etl_process
+
 
 app_server = app.server
 
 if __name__ == "__main__":
-    
-    os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'  
-    os.environ['HADOOP_HOME'] = '/opt/hadoop'  
-    os.environ['PYSPARK_PYTHON'] = sys.executable  
+    os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java' 
+    os.environ['HADOOP_HOME'] = '/opt/hadoop' 
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+
     df = etl_process("data/traffic_data.csv")
     model, predictions = train_model(df)
     explain_model(model, predictions)
